dd5ac1d6bb90f424d4df51cdcda2156875d373f8
FelixLjungqvist <felixljungqvist97@gmail.com>
2023-05-13 21:32:44 +0200

    Initial qwik preset conf

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -1,19 +1,39 @@
-import { component$ } from '@builder.io/qwik';
-import { useServerTimeLoader } from '~/routes/layout';
-import styles from './footer.module.css';
+import { component$ } from '@builder.io/qwik'
+import styles from './footer.module.css'
+import { useServerTimeLoader } from '~/routes/layout'
 
 export default component$(() => {
-  const serverTime = useServerTimeLoader();
+  const serverTime = useServerTimeLoader()
 
   return (
     <footer>
       <div class="container">
-        <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
-          <span>Made with ♡ by Builder.io</span>
-          <span class={styles.spacer}>|</span>
-          <span>{serverTime.value.date}</span>
-        </a>
+        <div class={styles.wrapper}>
+          <ul class={styles.linkList}>
+            <li>
+              <a class="hover:opacity-60" target="_blank" href="mailto:felixljungqvist@icloud.com">
+                Email
+              </a>
+            </li>
+            |
+            <li>
+              <a class="hover:opacity-60" target="_blank" href="https://twitter.com/LjungqvistFelix">
+                Twitter
+              </a>
+            </li>
+            |
+            <li>
+              <a class="hover:opacity-60" target="_blank" href="https://www.linkedin.com/in/felix-ljungqvist-422119181/">
+                LinkedIn
+              </a>
+            </li>
+          </ul>
+
+          <p class={styles.copyright}>
+            © <span>{serverTime.value.year}</span>
+          </p>
+        </div>
       </div>
     </footer>
-  );
-});
+  )
+})
--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,6 +1,6 @@
-import { component$ } from '@builder.io/qwik';
-import { QwikLogo } from '../icons/qwik';
-import styles from './header.module.css';
+import { component$ } from '@builder.io/qwik'
+import { QwikLogo } from '../icons/qwik'
+import styles from './header.module.css'
 
 export default component$(() => {
   return (
@@ -8,27 +8,22 @@ export default component$(() => {
       <div class={['container', styles.wrapper]}>
         <div class={styles.logo}>
           <a href="/" title="qwik">
-            <QwikLogo height={50} width={143} />
+            <QwikLogo height={40} width={143} />
           </a>
         </div>
         <ul>
           <li>
             <a href="https://qwik.builder.io/docs/components/overview/" target="_blank">
-              Docs
+              Work
             </a>
           </li>
           <li>
             <a href="https://qwik.builder.io/examples/introduction/hello-world/" target="_blank">
-              Examples
-            </a>
-          </li>
-          <li>
-            <a href="https://qwik.builder.io/tutorial/welcome/overview/" target="_blank">
-              Tutorials
+              About
             </a>
           </li>
         </ul>
       </div>
     </header>
-  );
-});
+  )
+})
